40f4be2684a5e762cfd107bf700c229993683578
xieshuhong <384180258@qq.com>
2025-09-01 18:31:05 +0800

    feat(ui): 增加SearchTable组件基础模板

---
+++ packages/ui/src/components/SearchTable/SearchTable.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted, defineProps, defineEmits } from 'vue'
+</script>
+
+<template>
+  <div class="component-name"></div>
+</template>
+
+<style lang="scss" scoped></style>
