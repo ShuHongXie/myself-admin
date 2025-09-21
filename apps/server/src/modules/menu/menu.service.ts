5771d1df606dbe2b71a9f4f652dcce469dd343f4
xieshuhong <384180258@qq.com>
2025-09-21 20:26:55 +0800

    feat(server): 增加获取菜单信息接口services

--- apps/server/src/modules/menu/menu.service.ts
+++ apps/server/src/modules/menu/menu.service.ts
@@ -113,50 +113,50 @@ export class MenuService {
       }))
   }
 
-  // async getInfo(req): Promise<Menu[]> {
-  //   //user.guard中注入的解析后的JWTtoken的user
-  //   const { user } = req
-  //   //根据关联关系通过user查询user下的菜单和角色
-  //   const userList: User = await this.userRepository
-  //     .createQueryBuilder('fs_user')
-  //     .leftJoinAndSelect('fs_user.roles', 'fs_role')
-  //     .leftJoinAndSelect('fs_role.menus', 'fs_menu')
-  //     .where({ id: user.userId })
-  //     .orderBy('fs_menu.order_num', 'ASC')
-  //     .getOne()
+  async getInfo(req): Promise<Menu[]> {
+    //user.guard中注入的解析后的JWTtoken的user
+    const { user } = req
+    //根据关联关系通过user查询user下的菜单和角色
+    const userList: User = await this.userRepository
+      .createQueryBuilder('fs_user')
+      .leftJoinAndSelect('fs_user.roles', 'fs_role')
+      .leftJoinAndSelect('fs_role.menus', 'fs_menu')
+      .where({ id: user.userId })
+      .orderBy('fs_menu.order_num', 'ASC')
+      .getOne()
 
-  //   //是否为超级管理员,是的话查询所有菜单
-  //   const isAdmin = userList.roles?.find((item) => item.role_name === 'admin')
-  //   let routers: Menu[] = []
+    //是否为超级管理员,是的话查询所有菜单
+    const isAdmin = userList.roles?.find((item) => item.role_name === 'admin')
+    let routers: Menu[] = []
 
-  //   if (isAdmin) {
-  //     routers = await this.menuRepository.find({
-  //       order: {
-  //         order_num: 'ASC'
-  //       },
-  //       where: {
-  //         status: 1
-  //       }
-  //     })
-  //     return convertToTree(routers)
-  //   }
-  //   interface MenuMap {
-  //     [key: string]: Menu
-  //   }
-  //   // console.log(userList.roles[0].menus);
+    if (isAdmin) {
+      routers = await this.menuRepository.find({
+        order: {
+          order_num: 'ASC'
+        },
+        where: {
+          status: 1
+        }
+      })
+      return convertToTree(routers)
+    }
+    interface MenuMap {
+      [key: string]: Menu
+    }
+    // console.log(userList.roles[0].menus);
 
-  //   //根据id去重
-  //   const menus: MenuMap = userList?.roles.reduce((mergedMenus: MenuMap, role: Role) => {
-  //     role.menus.forEach((menu: Menu) => {
-  //       mergedMenus[menu.id] = menu
-  //     })
-  //     return mergedMenus
-  //   }, {})
+    //根据id去重
+    const menus: MenuMap = userList?.roles.reduce((mergedMenus: MenuMap, role: Role) => {
+      role.menus.forEach((menu: Menu) => {
+        mergedMenus[menu.id] = menu
+      })
+      return mergedMenus
+    }, {})
 
-  //   routers = Object.values(menus)
+    routers = Object.values(menus)
 
-  //   return convertToTree(routers)
-  // }
+    return convertToTree(routers)
+  }
 
   /**
    * @description 根据ID获取菜单详情
