010a6ebf23510c28de58562e52c5f1497f0d03d1
xieshuhong <384180258@qq.com>
2025-12-15 17:37:36 +0800

    refactor: 去除web子包的无效构建配置

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -16,50 +16,7 @@ export default defineConfig({
     //css代码分割
     cssCodeSplit: true,
     //压缩
-    minify: false,
-    rollupOptions: {
-      //忽略打包vue文件
-      external: [
-        'vue',
-        'element-plus',
-        'unplugin-vue-components',
-        'unplugin-auto-import',
-        '@minilo/utils'
-      ],
-      input: ['./src/index.ts'],
-      output: [
-        {
-          //打包格式
-          format: 'es',
-          //打包后文件名
-          entryFileNames: '[name].mjs',
-          //让打包目录和我们目录对应
-          preserveModules: true,
-          preserveModulesRoot: 'src', // 从src文件夹开始打包
-          exports: 'named',
-          //配置打包根目录
-          dir: `./${outDir}/es`
-        },
-        {
-          //打包格式
-          format: 'cjs',
-          //打包后文件名
-          entryFileNames: '[name].js',
-          //让打包目录和我们目录对应
-          preserveModules: true,
-          preserveModulesRoot: 'src',
-          exports: 'named',
-          //配置打包根目录
-          dir: `./${outDir}/lib`
-        }
-      ]
-    },
-    lib: {
-      entry: './src/index.ts',
-      name: outDir,
-      fileName: 'index',
-      formats: ['es', 'cjs']
-    }
+    minify: false
   },
   css: {
     // 确保所有 CSS 都被正确处理
