c4b2a4e3f7cae920ba2c03906153e162e62f8de5
xieshuhong <384180258@qq.com>
2025-12-20 19:51:45 +0800

    test: 删除search组件

--- packages/ui/src/components/Search/index.ts
+++
@@ -1,6 +0,0 @@
-import { withInstall } from '../../utils'
-// export { default as MlSearch } from './index.vue'
-
-import Search from './index.vue'
-export const MlSearch = withInstall(Search)
-export default MlSearch
