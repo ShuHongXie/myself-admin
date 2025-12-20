e3d3cc195c8e9a1b9dd917d2bd14d3d9cf2aed4e
xieshuhong <384180258@qq.com>
2025-12-20 19:39:12 +0800

    test: 构建命令测试

--- packages/ui/src/components/Search/index.ts
+++ packages/ui/src/components/Search/index.ts
@@ -1,6 +1,6 @@
 import { withInstall } from '../../utils'
 // export { default as MlSearch } from './index.vue'
 
-import Search from '../search/index.vue'
+import Search from './index.vue'
 export const MlSearch = withInstall(Search)
 export default MlSearch
